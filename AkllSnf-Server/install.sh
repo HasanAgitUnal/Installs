5901c0e072f2e58b47230bbb0ede729a3bc92d51
HasanAgitUnal <hasanagitunal22@gmail.com>
2025-10-01 20:35:21 +0300

    Update install.sh

--- AkllSnf-Server/install.sh
+++ AkllSnf-Server/install.sh
@@ -5,10 +5,8 @@
     exit 1
 }
 
-if [[  $1 != "update" ]]; then
-    echo "Sunucu paketi indiriliyor..."
-    curl -so /tmp/pi-server.deb "https://raw.githubusercontent.com/HasanAgitUnal/Installs/master/AkllSnf-Server/pi-server.deb"
-fi
+echo "Sunucu paketi indiriliyor..."
+curl -so /tmp/pi-server.deb "https://raw.githubusercontent.com/HasanAgitUnal/Installs/master/AkllSnf-Server/pi-server.deb"
 
 echo "Eklenti indiriliyor..."
 curl -so /tmp/plugin.zip "https://raw.githubusercontent.com/HasanAgitUnal/Installs/master/AkllSnf-Server/plugin.zip"
